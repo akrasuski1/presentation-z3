01335296d3aee034f62320aa2806d8c060332347
akrasuski1 <akrasuski1@gmail.com>
2018-12-07 00:23:46 +0100

    Fixed bit hack example

--- examples/bit.py
+++ examples/bit.py
@@ -3,9 +3,9 @@ from z3 import *
 v, p = BitVecs('v p', 32)
 
 s = Solver()
-s.add(Not(Implies(
-    (v & (v - 1)) == 0,
+s.add(
+    ((v & (v - 1)) == 0) !=
     Exists(p, And(p>=0, p<32, (BitVecVal(1, 32) << p) == v))
-)))
+)
 print s.check()
 print s.model()
--- examples/bit_fixed.py
+++ examples/bit_fixed.py
@@ -3,8 +3,8 @@ from z3 import *
 v, p = BitVecs('v p', 32)
 
 s = Solver()
-s.add(Not(Implies(
-    And((v & (v - 1)) == 0, v != 0),
+s.add(
+    And((v & (v - 1)) == 0, v != 0) !=
     Exists(p, And(p>=0, p<32, (BitVecVal(1, 32) << p) == v))
-)))
+)
 print s.check()
